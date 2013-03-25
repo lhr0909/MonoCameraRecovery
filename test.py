01858650eacafe3b7f1884f57c0883e7af756111
Simon Liang <no.lhr0909@gmail.com>
2013-03-25 01:32:36 -0400

    Ditched SimpleCV and go full OpenCV
    Finished morphology AND corner detection

--- test.py
+++ test.py
@@ -1,28 +1,53 @@
-import SimpleCV
-import cv
-camera = SimpleCV.Camera(camera_index=0)
-camera.loadCalibration("default")
-print camera.getAllProperties()
-while True:
-    img = camera.getImageUndistort().smooth()
-#    peaks = img.huePeaks()
-#    hue = img.hueDistance(180).smooth('median').binarize(30)
-#    blobs = hue.findBlobs(minsize=5000)
-#    #blobs.draw(width=5)
-#    if blobs:
-#        blobs.image = img
-#        for blob in blobs:
-#            blob.drawMinRect(color=SimpleCV.Color.GREEN, width=5)
-#    corners = img.findCorners()
-#    if corners:
-#        corners.image = img
-#        for corner in corners:
-#            corner.draw(color=SimpleCV.Color.BLUE, width=1)
-#    img.show()
-    imgcv = img.getMatrix()
-    #create HSV image using OpenCV
-    hsv = cv.CreateImage((imgcv.cols, imgcv.rows), cv.IPL_DEPTH_8U, 3)
-    cv.CvtColor(imgcv, hsv, cv.CV_BGR2HSV)
+import cv2
+import numpy
+cv2.namedWindow("MonoCameraRecovery", cv2.CV_WINDOW_AUTOSIZE)
+camera = cv2.VideoCapture(0)
 
-    img2 = SimpleCV.Image(hsv)
-    img2.show()
+# Make sure you turn off the Auto White Balance,
+# Auto Exposure, and Auto Backlight Compensation
+# in the driver settings!
+
+while True:
+    [retval, img] = camera.read()
+    #convert to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
+    cv2.imwrite('hsv.png', hsv)
+
+    mask = cv2.inRange(hsv,
+        numpy.array([230, 150, 0], numpy.uint8),
+        numpy.array([255, 220, 255], numpy.uint8))
+
+#    #open then close
+#    se = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+#    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, se)
+#    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, se)
+
+    img_square = numpy.zeros(img[:,:,0].shape, numpy.uint8)
+
+    #Filter out the small noise
+    [contours, hierarchy] = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    for i in xrange(len(contours)):
+        area = cv2.contourArea(contours[i], False)
+        if area >= 13000:
+            cv2.drawContours(img_square, contours, i, [255, 255, 255], thickness=cv2.cv.CV_FILLED)
+
+    img_square = cv2.blur(img_square, (65,65))
+    #img_square = cv2.medianBlur(img_square, 51)
+
+    #Corner Detection
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    corners = cv2.goodFeaturesToTrack(img_gray, 100, 0.1, 50)
+
+    for c in corners:
+        #print img_square[c[0,1],c[0,0]]
+        if img_square[c[0,1],c[0,0]] > 0:
+            cv2.circle(img, tuple(c[0]), 2, [0, 255, 255])
+
+    cv2.imshow("MonoCameraRecovery", img)
+
+    key = cv2.waitKey(1)
+    if key == 13:
+        break
+
+camera.release()
