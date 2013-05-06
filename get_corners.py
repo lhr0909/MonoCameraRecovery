061ad2b1fc30f74ff384937a9cd338e3899dbd69
Simon Liang <no.lhr0909@gmail.com>
2013-05-05 22:45:46 -0400

    this param seems good

--- get_corners.py
+++ get_corners.py
@@ -4,7 +4,7 @@ def get_corners(img, img_square):
 #    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_gray = img[:,:,0] #use the blue channel
     square_corners = []
-    corners = cv2.goodFeaturesToTrack(img_gray, 100, 0.01, 30, useHarrisDetector=True, k=0.1)
+    corners = cv2.goodFeaturesToTrack(img_gray, 100, 0.05, 30, useHarrisDetector=True, k=0.1)
     if corners is not None and corners.shape[0] > 0:
         for c in corners:
             #print img_square[c[0,1],c[0,0]]
